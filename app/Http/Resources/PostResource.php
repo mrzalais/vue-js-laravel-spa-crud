ea1c7621f652303629078df8636711960cb0d46a
mrzalais <lejnieksedijs@gmail.com>
2023-03-18 19:54:14 +0200

    Add PostResource

---
+++ app/Http/Resources/PostResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var $this Post */
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => substr($this->content, 0, 50) . '...',
+            'created_at' => $this->created_at->toDateString(),
+        ];
+    }
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 class Post extends Model
 {
     use HasFactory;
