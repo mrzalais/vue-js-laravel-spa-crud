d64b0b74b03edceae011c6cc06139410490224e0
mrzalais <lejnieksedijs@gmail.com>
2023-03-18 14:43:00 +0200

    Add first component with dummy data

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,11 +2,16 @@ import './bootstrap';
 import '../css/app.css';
 
 import { createApp, h } from 'vue';
+import PostsIndex from './Components/Posts/Index.vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+const app = createApp({});
+app.component('posts-index', PostsIndex)
+app.mount('#app');
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
