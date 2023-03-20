1bcdf29e2a8f99073160b86259bb417572595910
mrzalais <lejnieksedijs@gmail.com>
2023-03-20 22:20:06 +0200

    Add column ordering

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,10 +11,21 @@ class PostController extends Controller
 {
     public function index(): AnonymousResourceCollection
     {
+        $orderColumn = request('order_column', 'created_at');
+        if (!in_array($orderColumn, ['id', 'title', 'created_at'])) {
+            $orderColumn = 'created_at';
+        }
+
+        $orderDirection = request('order_direction', 'desc');
+        if (!in_array($orderDirection, ['asc', 'desc'])) {
+            $orderDirection = 'created_at';
+        }
+
         $posts = Post::query()->with('category')
             ->when(request('category'), function ($query) {
                 $query->where('category_id', request('category'));
             })
+            ->orderBy($orderColumn, $orderDirection)
             ->paginate(10);
         return PostResource::collection($posts);
     }
--- resources/js/Composables/posts.js
+++ resources/js/Composables/posts.js
@@ -3,8 +3,18 @@ import { ref } from 'vue';
 export default function usePosts() {
     const posts = ref({})
 
-    const getPosts = async (page = 1, category = '') => {
-        const response = await fetch(`/api/posts?page=${page}&category=${category}`);
+    const getPosts = async (
+        page = 1,
+        category = '',
+        order_column = 'created_at',
+        order_direction = 'desc',
+    ) => {
+        const response = await fetch(`/api/posts?
+        page=${page}
+        &category=${category}
+        &order_column=${order_column}
+        &order_direction=${order_direction}
+        `);
         posts.value = await response.json();
     }
 
